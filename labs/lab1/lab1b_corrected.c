8f3398c50cffb30ac52a2e924ad12eaa8c368539
Lee Wei Min <40821628+leeweiminsg@users.noreply.github.com>
2020-01-31 21:23:05 +0800

    lab1 work

---
+++ labs/lab1/lab1b_corrected.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <string.h>
+#define MAX 10
+
+int readArray(int[], int);
+void printArray(int[], int);
+void reverseArray(int[], size_t);
+
+int main(void)
+{
+  int array[MAX], numElements;
+
+  numElements = readArray(array, MAX);
+  reverseArray(array, numElements);
+  printArray(array, numElements);
+
+  return 0;
+}
+
+int readArray(int arr[], int limit)
+{
+  int i, input;
+
+  printf("Enter up to %d integers, terminating with a negative integer.\n", limit);
+  i = 0;
+  scanf("%d", &input);
+  while (input >= 0)
+  {
+    arr[i] = input;
+    i++;
+    scanf("%d", &input);
+  }
+  return i;
+}
+
+void reverseArray(int arr[], size_t size)
+{
+  int *left = arr;
+  int *right = arr + size - 1;
+  while (left < right)
+  {
+    int tmp = *left;
+    *left = *right;
+    *right = tmp;
+    left++;
+    right--;
+  }
+}
+
+void printArray(int arr[], int size)
+{
+  int i;
+
+  for (i = 0; i < size; i++)
+  {
+    printf("%d ", arr[i]);
+  }
+  printf("\n");
+}
